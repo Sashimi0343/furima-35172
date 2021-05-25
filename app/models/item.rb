9f26fb976ad3bf0fad8e742e640b00fe0dfac940
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-05-25 20:35:15 +0900

    トップページに商品一覧表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,5 @@
 class Item < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :days_to_ship, :delivery_charge, :prefecture, :status
-
 
   with_options presence: true do
     validates :title
@@ -10,7 +7,6 @@ class Item < ApplicationRecord
     validates :price, format: { with: /\A[0-9]+\z/ }
     validates_inclusion_of :price, in:300..9999999, message: "Please enter 300 〜 9,999,999 & half-width numbers"
     validates :image
-
 
     with_options numericality: { other_than: 0, message: 'Please choose an option.' } do
       validates :category_id
@@ -20,6 +16,14 @@ class Item < ApplicationRecord
       validates :days_to_ship_id
     end
   end
+
+  belongs_to :category
+  belongs_to :days_to_ship
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :status
+
   belongs_to :user
   has_one_attached :image
+
 end
