af9c3ab14effbd5876c5aadb68b0036552dd0d41
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-05-25 19:35:18 +0900

    エラーメッセージ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,12 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :description
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates_inclusion_of :price, in:300..9999999, message: "Please enter 300 〜 9,999,999 & half-width numbers"
     validates :image
 
 
-    with_options numericality: { other_than: 0, message: 'test' } do
+    with_options numericality: { other_than: 0, message: 'Please choose an option.' } do
       validates :category_id
       validates :status_id
       validates :delivery_charge_id
