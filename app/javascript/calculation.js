98aef05f0ece53dba3a47653d889d708e062aabe
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-05-26 15:39:20 +0900

    手数料・純利益自動計算機能実装

---
+++ app/javascript/calculation.js
@@ -0,0 +1,19 @@
+function calculation() {
+
+  const item_price = document.getElementById("item-price");
+  item_price.addEventListener("keyup", () => {
+
+    console.log(item_price.value)
+    const price = item_price.value;
+
+    const fee = document.getElementById("add-tax-price");
+    const fee_tax = price / 10
+    fee.innerHTML = `${Math.floor(fee_tax)}`;
+
+    const profit = document.getElementById("profit");
+    const income = price - fee_tax
+    profit.innerHTML = `${Math.ceil(income)}`;
+  });
+}
+
+window.addEventListener("load", calculation);
