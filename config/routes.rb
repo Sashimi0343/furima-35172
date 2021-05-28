aaf25e58c53e2b5026b92cc37900761113d967d8
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-05-29 01:27:58 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  resources :items
-
+  resources :items do
+    resources :orders, only: [:index, :create] do
+      resources :shippings, only: [:create]
+    end
+  end
 end
