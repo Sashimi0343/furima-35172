dd6692074ed72c43088ff56b53e5a946be9cd6e7
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-04 00:20:08 +0900

    単体テスト実装

---
+++ spec/factories/order_shippings.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_shipping do
+
+    postal_code {'123-4567'}
+    prefecture_id { 1 }
+    municipality { '川口市石神' }
+    address {' 977-1 '}
+    building {' アムール新町102 '}
+    phone_number { '08054864124' }
+    token {ENV["PAYJP_PUBLIC_KEY"]}
+  end
+end
