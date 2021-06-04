5be6412e5fc53fa6d9ef645315a1c0e3599d31df
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-04 15:49:06 +0900

    指摘箇所修正３

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :create]
-  before_action :set_item, only: [:index, :create]
-  before_action :check_user, only: [:index, :create]
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :check_user
 
 
   def index
