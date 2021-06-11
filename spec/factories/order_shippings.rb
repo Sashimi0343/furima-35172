fc92cf2ddcea1597b3a4420d4b0cf104629c2b8e
Sashimi0343 <81734876+Sashimi0343@users.noreply.github.com>
2021-06-11 19:34:15 +0900

    提出用に修正

--- spec/factories/order_shippings.rb
+++ spec/factories/order_shippings.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :order_shipping do
     postal_code { '123-4567' }
     prefecture_id { 1 }
-    municipality { '川口市石神' }
-    address { ' 977-1 ' }
-    building { ' アムール新町102 ' }
-    phone_number { '08054864124' }
+    municipality { '市区町村' }
+    address { ' 番地 ' }
+    building { ' 建物 ' }
+    phone_number { '08012345678' }
     token { ENV['PAYJP_PUBLIC_KEY'] }
   end
 end
